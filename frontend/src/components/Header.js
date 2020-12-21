a6b5b8e2f0f2043c46cde43007b6b1e2521b7e18
pic2725 <pic2725@gmail.com>
2020-12-20 19:13:56 -0700

    updated for deploy

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import React from "react";
 import { Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
@@ -28,7 +27,6 @@ const Header = () => {
     }
     console.log(navbar);
   };
-
   window.addEventListener("scroll", changeBackground);
 
   return (
